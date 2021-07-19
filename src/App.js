b4a5fec7728486bdb067d40a40e000a8b7fd2295
Mohsen Fallahnejad <flashfa8@gmail.com>
2021-07-20 00:35:41 +0430

    Refactor App component and add some routes and ViewWrapper component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React, { useContext } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { createTheme, ThemeProvider, SwipeableDrawer } from "@material-ui/core";
+
+import Home from "./views/Home";
+import Blog from "./views/Blog";
+import Login from "./views/Login";
+import Register from "./views/Register";
+
+import { createTheme, ThemeProvider } from "@material-ui/core";
+import ViewsWrapper from "./ViewsWrapper";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { lightGreen } from "@material-ui/core/colors";
-import Home from "./views/Home";
-import AppContext from "./store/app-context";
 
 function App() {
-  const ctx = useContext(AppContext);
-
   const theme = React.useMemo(
     () =>
       createTheme({
@@ -17,7 +20,7 @@ function App() {
           type: "dark",
           background: {
             default: "#1b1d21",
-            paper: '#1b1d21'
+            paper: "#1b1d21",
             // paper: "#292c31",
           },
           text: {
@@ -33,19 +36,22 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <SwipeableDrawer
-        anchor="left"
-        open={ctx.showDrawer}
-        onOpen={() => ctx.toggleShowDrawer()}
-        onClose={() => ctx.toggleShowDrawer()}
-      >
-        <h1>Hello World</h1>
-      </SwipeableDrawer>
       <Router>
         <Switch>
-          <Route path="/">
-            <Home />
-          </Route>
+          <ViewsWrapper>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/blog">
+              <Blog />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+          </ViewsWrapper>
         </Switch>
       </Router>
     </ThemeProvider>
