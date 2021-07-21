ea0854d11fb1dc3d1539aadad2f2e5da43f5997d
Mohsen Fallahnejad <flashfa8@gmail.com>
2021-07-21 23:41:00 +0430

    create LocalStorage class

---
+++ src/adapters/LocalStorage.js
@@ -0,0 +1,77 @@
+export default class LocalStorage {
+  dbName = "";
+
+  constructor(dbName) {
+    this.dbName = dbName;
+    if (!this.dbExist()) {
+      localStorage.setItem(this.dbName, "[]");
+    }
+  }
+
+  dbExist() {
+    return localStorage.getItem(this.dbName) !== null;
+  }
+
+  getItems() {
+    if (this.dbExist()) {
+      return JSON.parse(localStorage.getItem(this.dbName));
+    }
+    return [];
+  }
+
+  setItems(items) {
+    localStorage.setItem(this.dbName, JSON.stringify(items));
+  }
+
+  getItem(itemId) {
+    const items = this.getItems();
+    const [item] = items.filter((itemArg) => itemArg.id === itemId);
+    return item;
+  }
+
+  addItem(item) {
+    if (!this.dbExist()) return false;
+    const items = this.getItems();
+    items.push(item);
+    this.setItems(items);
+    return true;
+  }
+
+  itemExist(itemId) {
+    if (!this.dbExist()) return false;
+    const items = this.getItems();
+    const itemIndex = items.findIndex((itemArg) => itemArg.id === itemId);
+    return itemIndex > -1;
+  }
+
+  removeItem(itemId) {
+    if (!this.dbExist()) return false;
+    const items = this.getItems();
+    const filteredItems = items.filter((itemArg) => itemArg.id !== itemId);
+    this.setItems(filteredItems);
+    return true;
+  }
+
+  updateItem(item) {
+    if (!this.dbExist()) return false;
+    const items = this.getItems();
+    const itemIndex = items.findIndex((itemArg) => itemArg.id === item.id);
+    items[itemIndex] = item;
+    this.setItems(items);
+    return true;
+  }
+
+  emailExist(email) {
+    const items = this.getItems();
+    const itemIndex = items.findIndex((itemArg) => itemArg.email === email);
+    return itemIndex > -1;
+  }
+
+  saveUserData(data) {
+    localStorage.setItem(this.dbName, JSON.stringify(data));
+  }
+
+  getUserData() {
+    return JSON.parse(localStorage.getItem(this.dbName));
+  }
+}
