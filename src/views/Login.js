6603ccf1f9f46ccdf9a984c0f07654e5ed870703
Mohsen Fallahnejad <flashfa8@gmail.com>
2021-07-22 00:05:47 +0430

    add form validation and login functionality

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router";
 import {
   TextField,
@@ -15,6 +15,9 @@ import {
 
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import FAIcon from "../components/include/FontAwesomeIcon";
+import { useForm } from "react-hook-form";
+import LocalStorage from "../adapters/LocalStorage";
+import Message from "../components/include/Message";
 
 const useStyles = makeStyles({
   root: {
@@ -27,10 +30,24 @@ const useStyles = makeStyles({
   },
 });
 
+const ls = new LocalStorage("app-users");
+
 const Login = () => {
   const classes = useStyles();
   const [showPassword, setShowPassword] = useState(false);
   const history = useHistory();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+  const [message, setMessage] = useState({
+    show: false,
+    messages: [],
+    duration: 4000,
+    status: "error",
+  });
 
   const handleSubmitForm = (event) => {
     event.preventDefault();
@@ -40,54 +57,131 @@ const Login = () => {
     setShowPassword((prevVal) => !prevVal);
   };
 
+  const handleLoginBtnClick = (data) => {
+    const user = ls.getUserByEmail(data.email);
+    let messagesss = "Email or password is wrong!";
+    let hasError = true;
+    if (user && user.password === data.password) {
+      const userData = new LocalStorage("app-user-data");
+      userData.saveUserData({
+        id: user.id,
+        permission: user.permision,
+        userName: user.firstName + " " + user.lastName,
+      });
+
+      messagesss = "Welcome back babe :D";
+      hasError = false;
+    }
+
+    setMessage((prevData) => {
+      return {
+        ...prevData,
+        show: true,
+        status: hasError ? "error" : "success",
+        messages: [messagesss],
+      };
+    });
+
+    setTimeout(() => {
+      setMessage((prevData) => {
+        return {
+          ...prevData,
+          show: false,
+          status: "error",
+          messages: [],
+        };
+      });
+    }, message.duration + 100);
+
+    console.log("user is not valid!");
+  };
+
   return (
-    <Card className={classes.root}>
-      <CardContent>
-        <Typography variant="h5">Login Form</Typography>
-        <form action="#" onSubmit={handleSubmitForm}>
-          <Box>
-            <TextField
-              type="email"
-              id="email"
-              label="Email"
-              fullWidth
-              autoComplete="off"
-            />
-          </Box>
-          <Box margin="10px 0 0 0">
-            <TextField
-              type={showPassword ? "text" : "password"}
-              id="password"
-              label="password"
-              fullWidth
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <FAIcon
-                      icon={showPassword ? faEye : faEyeSlash}
-                      onClick={handleClickPasswordIcon}
-                    />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Box>
-        </form>
-      </CardContent>
-      <Divider />
-      <CardActions style={{ justifyContent: "space-between" }}>
-        <Button color="primary" variant="contained">
-          Login
-        </Button>
-        <Button
-          color="secondary"
-          variant="contained"
-          onClick={() => history.push("/register")}
-        >
-          Register
-        </Button>
-      </CardActions>
-    </Card>
+    <React.Fragment>
+      {message.show && (
+        <Message
+          messages={message.messages}
+          duration={message.duration}
+          status={message.status}
+        />
+      )}
+      <Card className={classes.root}>
+        <CardContent>
+          <Typography variant="h5">Login Form</Typography>
+          <form action="#" onSubmit={handleSubmitForm}>
+            <Box>
+              <TextField
+                type="email"
+                id="email"
+                label="Email"
+                fullWidth
+                autoComplete="off"
+                {...register("email", {
+                  required: {
+                    value: true,
+                    message: "Please enter your email",
+                  },
+                  pattern: {
+                    value:
+                      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                    message: "email is not valid",
+                  },
+                })}
+                error={errors.email ? true : false}
+                helperText={(errors.email && errors.email.message) || ""}
+              />
+            </Box>
+            <Box margin="10px 0 0 0">
+              <TextField
+                type={showPassword ? "text" : "password"}
+                id="password"
+                label="password"
+                fullWidth
+                {...register("password", {
+                  required: {
+                    value: true,
+                    message: "Please enter your password",
+                  },
+                  pattern: {
+                    value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+                    message: "password is not valid",
+                  },
+                })}
+                error={errors.password ? true : false}
+                helperText={(errors.password && errors.password.message) || ""}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <FAIcon
+                        icon={showPassword ? faEye : faEyeSlash}
+                        onClick={handleClickPasswordIcon}
+                      />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Box>
+          </form>
+        </CardContent>
+        <Divider />
+        <CardActions style={{ justifyContent: "space-between" }}>
+          <Button
+            color="primary"
+            variant="contained"
+            onClick={handleSubmit(handleLoginBtnClick)}
+          >
+            Login
+          </Button>
+          <Button
+            color="secondary"
+            variant="contained"
+            onClick={() => history.push("/register")}
+          >
+            Register
+          </Button>
+        </CardActions>
+      </Card>
+    </React.Fragment>
   );
 };
 
