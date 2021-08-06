8f1a6dfd60cf44b10786fc2ffdaa0b85ccd63cb9
Mohsen Falahnejad <rojaweb1@gmail.com>
2021-08-06 10:57:17 -0700

    create PostDAO

---
+++ src/DB/PostDAO.js
@@ -0,0 +1,24 @@
+import db from './db';
+
+export class PostDAO {
+
+    createPost(post) {
+        db.posts.put(post)
+    }
+
+    async updatePost(post) {
+        return db.posts.update(post.id, post)
+    }
+
+    async deletePost(postId) {
+        return db.posts.delete(postId)
+    }
+
+    async getPost(postId) {
+        return db.posts.get(postId);
+    }
+
+    async getPosts() {
+        return db.posts.toArray();
+    }
+}
---
+++ src/DB/db.js
@@ -0,0 +1,30 @@
+import Dexie from "dexie";
+import {AccountDTO} from "../adapters/AccountDTO";
+
+const db = new Dexie('MyDataBase');
+db.version(1).stores({
+    accounts: "id, firstName, lastName, username, email, password, permission",
+    posts: "id, thumbnail, title, summary, content, *tags, visits, *comments, *categories",
+    comments: "id, postId, name, email, content",
+    categories: "id, thumbnail, title, isPublic",
+})
+
+async function configuration() {
+    const data = await db.accounts.get({username: 'mohsen_coder'})
+    if (!data) {
+        const accountDTO = new AccountDTO({
+            firstName: 'Mohsen',
+            lastName: 'Falahnejad',
+            username: 'mohsen_coder',
+            email: 'test@gmail.com',
+            password: '76mnoZxA',
+            permission: 'admin'
+        });
+
+        db.accounts.put(accountDTO)
+    }
+}
+
+configuration();
+
+export default db;
