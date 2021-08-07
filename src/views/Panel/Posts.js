06bdbd9593f0f3ac5b1cf10f04fff432af6fea68
Mohsen Falahnejad <rojaweb1@gmail.com>
2021-08-07 06:27:33 -0700

    edit and delete post functionality added to Posts component

--- src/views/Panel/Posts.js
+++ src/views/Panel/Posts.js
@@ -18,11 +18,12 @@ import {
 } from "@material-ui/core";
 import CustomButton from "../../adapters/CustomButton";
 import {PostDAO} from "../../DB/PostDAO";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import CustomIconButton from "../../adapters/CustomIconButton";
 import FAIcon from '../../components/include/FontAwesomeIcon';
-import {faEdit} from "@fortawesome/free-solid-svg-icons";
+import {faEdit, faTrash} from "@fortawesome/free-solid-svg-icons";
 import {Alert} from "@material-ui/lab";
+import Message from "../../components/include/Message";
 
 
 const useStyles = makeStyles({
@@ -33,27 +34,65 @@ const useStyles = makeStyles({
 
 const postDAO = new PostDAO();
 
+const PrimaryButton = new CustomButton('primary');
+const WarningIconButton = new CustomIconButton('warning');
+const ErrorIconButton = new CustomIconButton('error');
+
 const Posts = () => {
     const classes = useStyles();
-    const PrimaryButton = new CustomButton('primary');
-    const WarningIconButton = new CustomIconButton('warning');
     const history = useHistory();
     const {path} = useRouteMatch();
     const util = useUtil();
     const [posts, setPosts] = useState([]);
+    const [message, setMessage] = useState({
+        show: false,
+        messages: [],
+        duration: 4000,
+        status: "error",
+    });
+    const PARENT_ROUTE = util.routeParent(path);
+
+    const getPosts = async () => {
+        const posts = await postDAO.getPosts();
+        setPosts(posts)
+    }
 
     useEffect(() => {
-        postDAO.getPosts().then(posts => {
-            setPosts(posts)
+        getPosts();
+    }, []);
+
+    const handleDeletePost = (postId) => {
+        postDAO.deletePost(postId).then(() => {
+            setMessage(prevState => {
+                return {
+                    ...prevState,
+                    show: true,
+                    status: 'success',
+                    messages: ['Post deleted successfully']
+                }
+            })
+            getPosts();
         })
-    }, [])
+    }
 
     return (
         <Container>
+            <Message
+                show={message.show}
+                onClose={() => {
+                    setMessage(prevState => {
+                        return {...prevState, show: false}
+                    })
+                }
+                }
+                messages={message.messages}
+                duration={message.duration}
+                status={message.status}
+            />
             <Grid container>
                 <Grid item xs={12}>
                     <Box margin="20px 0">
-                        <PrimaryButton onClick={() => history.push(`${util.routeParent(path)}/new-post`)}>New
+                        <PrimaryButton onClick={() => history.push(`${PARENT_ROUTE}/new-post`)}>New
                             Post</PrimaryButton>
                     </Box>
                 </Grid>
@@ -82,9 +121,12 @@ const Posts = () => {
                                                     <TableCell>{post.visits}</TableCell>
                                                     <TableCell>{post.comments.length}</TableCell>
                                                     <TableCell>
-                                                        <WarningIconButton>
+                                                        <WarningIconButton onClick={() => history.push(`${PARENT_ROUTE}/edit-post/${post.id}`)}>
                                                             <FAIcon icon={faEdit} fontSize="sm"/>
                                                         </WarningIconButton>
+                                                        <ErrorIconButton onClick={() => handleDeletePost(post.id)}>
+                                                            <FAIcon icon={faTrash} fontSize="sm"/>
+                                                        </ErrorIconButton>
                                                     </TableCell>
                                                 </TableRow>
                                             )
@@ -104,7 +146,7 @@ const Posts = () => {
                                     </TableFooter>}
                                 </Table>
                             </TableContainer>
-                            {posts.length === 0 && <Alert variant="filled" severity="error">There is no post!</Alert> }
+                            {posts.length === 0 && <Alert variant="filled" severity="error">There is no post!</Alert>}
                         </CardContent>
                     </Card>
                 </Grid>
