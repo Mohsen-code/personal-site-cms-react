34746ed890dce6ce285c44030a5fcd3beba3fd7c
Mohsen Falahnejad <rojaweb1@gmail.com>
2021-08-08 00:59:32 -0700

    mapSpecificDataFromArray generic method added to util hook

--- src/hooks/util.js
+++ src/hooks/util.js
@@ -4,8 +4,24 @@ const useUtil = () => {
         return route.substring(0, route.lastIndexOf('/'));
     }
 
+    const mapSpecificDataFromArray = (shortArray, longArray, searchProp, mainProp) => {
+        const mappedData = [];
+
+        for (let shortArrayIndex = 0; shortArrayIndex < shortArray.length; shortArrayIndex++){
+            for (let longArrayIndex = 0; longArrayIndex < longArray.length; longArrayIndex++){
+                if (shortArray[shortArrayIndex] === longArray[longArrayIndex].[searchProp]){
+                    mappedData.push(longArray[longArrayIndex].[mainProp]);
+                }
+            }
+        }
+
+        return mappedData;
+    }
+
     return {
-        routeParent
+        routeParent,
+        mapSpecificDataFromArray
+
     }
 }
 
