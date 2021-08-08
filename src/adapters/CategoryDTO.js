c0de55cbec376d8bef0a5c7972afd5c17e969ae7
Mohsen Falahnejad <rojaweb1@gmail.com>
2021-08-07 23:00:43 -0700

    constructor added to CategoryDTO

--- src/adapters/CategoryDTO.js
+++ src/adapters/CategoryDTO.js
@@ -1,7 +1,12 @@
 import {v4 as uuid} from 'uuid'
-export class CategoryDTO{
+
+export class CategoryDTO {
     id = uuid().replace(/-/g, '');
     thumbnail = null;
     title = "";
     isPublic = false;
+
+    constructor(init) {
+        if (init) Object.assign(this, init)
+    }
 }
