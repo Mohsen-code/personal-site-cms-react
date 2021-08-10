11213447c7dd3ff51dbd1edd23210c5045085baf
Mohsen Falahnejad <rojaweb1@gmail.com>
2021-08-10 00:03:50 -0700

    CommentDAO created

---
+++ src/DB/CommentDAO.js
@@ -0,0 +1,25 @@
+import db from './db'
+
+export class CommentDAO{
+
+    createComment(comment){
+        db.comments.put(comment)
+    }
+
+    updateComment(comment){
+        db.comments.update(comment.id, comment)
+    }
+
+    deleteComment(commentId){
+        db.comments.delete(commentId)
+    }
+
+    async getComment(commentId){
+        return db.comments.get(commentId)
+    }
+
+    async getComments(isPublic){
+        return db.comments.toCollection().filter(comment => comment.isPublic === isPublic).toArray();
+    }
+
+}
