d4b83c0700df74a391cdac826637cfc8722ba87b
Mohsen Falahnejad <rojaweb1@gmail.com>
2021-08-10 00:13:04 -0700

    ContactUs view created

---
+++ src/views/ContactUs.js
@@ -0,0 +1,17 @@
+import {Container, Grid, TextField, Card, CardContent, CardActions, makeStyles} from "@material-ui/core";
+
+export const ContactUs = () => {
+    return (
+        <Container>
+            <Grid container>
+                <Grid item xs={12}>
+                    <Card>
+                        <CardContent>
+                            ContactUS
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Grid>
+        </Container>
+    )
+}
