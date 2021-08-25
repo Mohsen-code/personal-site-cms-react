71afdd65a5f79a533f1f132cbad17db78e291ab4
Mohsen Falahnejad <rojaweb1@gmail.com>
2021-08-25 12:31:22 -0700

    number of posts and comments added to Dashboard

--- src/views/Panel/Dashboard.js
+++ src/views/Panel/Dashboard.js
@@ -15,8 +15,10 @@ import {
 import { faUser, faEnvelope, faInbox, faComments, faMailBulk } from "@fortawesome/free-solid-svg-icons";
 import FAIcon from "../../components/include/FontAwesomeIcon";
 import TodoList from "../../components/Panel/Dashboard/TodoList";
-import {useEffect, useContext} from "react";
+import {useEffect, useContext, useState, useCallback} from "react";
 import AppContext from "../../store/app-context";
+import {PostDAO} from "../../DB/PostDAO";
+import {CommentDAO} from "../../DB/CommentDAO";
 
 const useStyles = makeStyles({
   root: {
@@ -24,9 +26,22 @@ const useStyles = makeStyles({
   },
 });
 
+const postDAO = new PostDAO();
+const commentDAO = new CommentDAO();
+
 const Dashboard = () => {
   const classes = useStyles();
   const {account} = useContext(AppContext)
+  const [postsCount, setPostsCount] = useState(0);
+  const [commentsCount, setCommentsCount] = useState(0);
+
+  const getPostsAndCommentsCount = useCallback(async () => {
+    setPostsCount(await postDAO.getPostsCount())
+    setCommentsCount(await commentDAO.getCommentsCount())
+  }, [])
+  useEffect(() => {
+    getPostsAndCommentsCount()
+  }, [getPostsAndCommentsCount])
 
   return (
     <Container>
@@ -60,14 +75,14 @@ const Dashboard = () => {
                           <ListItemIcon>
                             <FAIcon icon={faInbox} fontSize="lg" />
                           </ListItemIcon>
-                          <ListItemText>Posts Number: 23</ListItemText>
+                          <ListItemText>Posts Number: {postsCount}</ListItemText>
                         </ListItem>
                         <Divider />
                         <ListItem>
                           <ListItemIcon>
                             <FAIcon icon={faComments} fontSize="lg" />
                           </ListItemIcon>
-                          <ListItemText>Comments Number: 23</ListItemText>
+                          <ListItemText>Comments Number: {commentsCount}</ListItemText>
                         </ListItem>
                         <Divider />
                         <ListItem>
