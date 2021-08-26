e1408716cecc9f39c9f2360e55673eac1baadcf6
Mohsen Falahnejad <rojaweb1@gmail.com>
2021-08-26 14:17:06 -0700

    create contact us form

--- src/views/ContactUs.js
+++ src/views/ContactUs.js
@@ -1,14 +1,54 @@
-import {Container, Grid, TextField, Card, CardContent, CardActions, makeStyles} from "@material-ui/core";
+import {Container, Grid, TextField, Card, CardContent, CardActions, makeStyles, Box, Divider} from "@material-ui/core";
+import CustomButton from "../adapters/CustomButton";
+
+const useStyle = makeStyles({
+    card: {
+        backgroundColor: "#292c31",
+        marginTop: '20px'
+    }
+})
+
+const PrimaryButton = new CustomButton('primary')
 
 export const ContactUs = () => {
+    const classes = useStyle()
     return (
         <Container>
             <Grid container>
                 <Grid item xs={12}>
-                    <Card>
+                    <Card className={classes.card}>
                         <CardContent>
-                            ContactUS
+                            <Box>
+                                <TextField
+                                    label={`Name`}
+                                    fullWidth
+                                />
+                            </Box>
+                            <Box margin={'10px 0'}>
+                                <TextField
+                                    label={`Email`}
+                                    fullWidth
+                                />
+                            </Box>
+                            <Box margin={'10px 0'}>
+                                <TextField
+                                    label={`Title`}
+                                    fullWidth
+                                />
+                            </Box>
+                            <Box margin={'10px 0'}>
+                                <TextField
+                                    label={`Content`}
+                                    multiline
+                                    rows={10}
+                                    fullWidth
+                                />
+                            </Box>
                         </CardContent>
+                        
+                        <CardActions>
+                            <PrimaryButton>Send Message</PrimaryButton>
+                        </CardActions>
                     </Card>
                 </Grid>
             </Grid>
