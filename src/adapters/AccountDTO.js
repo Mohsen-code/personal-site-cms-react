ccb13c32c56a4c87e600627b7524bb1a15231b07
Mohsen Falahnejad <rojaweb1@gmail.com>
2021-08-27 08:55:56 -0700

    add createDate and thumbnail to AccountDTO

--- src/adapters/AccountDTO.js
+++ src/adapters/AccountDTO.js
@@ -2,11 +2,13 @@ import {v4 as uuid} from 'uuid';
 
 export class AccountDTO {
     id = uuid().replace(/-/g, '');
+    thumbnail = null
     firstName = ""
     lastName = ""
     email = ""
     password = ""
     permission = ""
+    createDate = new Date().getTime()
 
     constructor(init) {
         if (init) Object.assign(this, init)
